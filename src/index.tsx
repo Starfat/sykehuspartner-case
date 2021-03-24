a260d0bdec7b8b200764897bf42fc6b44a81445f
Alf Almen <alf.almen@starfat.com>
2021-03-24 20:13:58 +0100

    First Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import KomponentA from './components/KomponentA';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KomponentA />
   </React.StrictMode>,
   document.getElementById('root')
 );
